a9d0d9ff7b93980eecbf70e3ad5181572509f8d3
liuxiaofeng <13602490352@163.com>
2019-01-03 18:12:06 +0800

    后台众筹商品管理

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -83,7 +83,7 @@ class ProductsController extends Controller
     {
         return Admin::grid(Product::class, function (Grid $grid) {
             // 使用 with 来预加载商品类目数据，减少 SQL 查询
-            $grid->model()->with(['category']);
+            $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
             $grid->id('ID')->sortable();
             $grid->title('商品名称');
             // Laravel-Admin 支持用符号 . 来展示关联关系的字段
@@ -142,6 +142,8 @@ class ProductsController extends Controller
     protected function form()
     {
         return Admin::form(Product::class, function (Form $form) {
+            // 在表单中添加一个名为 type，值为 Product::TYPE_NORMAL 的隐藏字段
+            $form->hidden('type')->value(Product::TYPE_NORMAL);
             $form->text('title', '商品名称')->rules('required');
             // 添加一个类目字段，与之前类目管理类似，使用 Ajax 的方式来搜索添加
             $form->select('category_id', '类目')->options(function ($id) {
--- database/admin.sql
+++ database/admin.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-12-20 06:41:12'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,'2019-01-03 06:27:10'),(3,2,8,'管理员','fa-users','auth/users',NULL,'2019-01-03 06:27:10'),(4,2,9,'角色','fa-user','auth/roles',NULL,'2019-01-03 06:27:10'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,'2019-01-03 06:27:10'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,'2019-01-03 06:27:10'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,'2019-01-03 06:27:10'),(8,0,2,'用户管理','fa-users','/users','2018-12-20 07:21:50','2018-12-20 10:00:10'),(9,0,4,'商品管理','fa-cubes','/products','2018-12-20 09:59:17','2019-01-03 06:27:10'),(10,0,5,'订单管理','fa-rmb','/orders','2018-12-24 07:49:08','2019-01-03 06:27:10'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes','2018-12-28 06:20:06','2019-01-03 06:27:10'),(12,0,3,'类目管理','fa-bars','/categories','2019-01-03 06:26:23','2019-01-03 06:27:10');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-12-20 06:41:12'),(2,0,9,'系统管理','fa-tasks',NULL,NULL,'2019-01-03 10:03:38'),(3,2,10,'管理员','fa-users','auth/users',NULL,'2019-01-03 10:03:38'),(4,2,11,'角色','fa-user','auth/roles',NULL,'2019-01-03 10:03:38'),(5,2,12,'权限','fa-ban','auth/permissions',NULL,'2019-01-03 10:03:38'),(6,2,13,'菜单','fa-bars','auth/menu',NULL,'2019-01-03 10:03:38'),(7,2,14,'操作日志','fa-history','auth/logs',NULL,'2019-01-03 10:03:38'),(8,0,2,'用户管理','fa-users','/users','2018-12-20 07:21:50','2018-12-20 10:00:10'),(9,0,4,'商品管理','fa-cubes','/products','2018-12-20 09:59:17','2019-01-03 06:27:10'),(10,0,7,'订单管理','fa-rmb','/orders','2018-12-24 07:49:08','2019-01-03 10:03:38'),(11,0,8,'优惠券管理','fa-tags','/coupon_codes','2018-12-28 06:20:06','2019-01-03 10:03:38'),(12,0,3,'类目管理','fa-bars','/categories','2019-01-03 06:26:23','2019-01-03 06:27:10'),(13,9,6,'众筹商品','fa-flag-checkered','/crowdfunding_products','2019-01-03 10:02:50','2019-01-03 10:03:38'),(14,9,5,'普通商品','fa-cubes','/products','2019-01-03 10:03:28','2019-01-03 10:03:38');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-03  6:35:11
+-- Dump completed on 2019-01-03 10:11:56
