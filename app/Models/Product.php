f4400631764179fed8f5d4eaae124713698bed77
liuxiaofeng <13602490352@163.com>
2019-01-09 16:01:35 +0800

    商品添加长标题字段

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,8 @@ class Product extends Model
     ];
 
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
+        'title', 'long_title', // 添加 long_title 到 $fillable 属性中
+        'description', 'image', 'on_sale',
         'rating', 'sold_count', 'review_count', 'price',
         'type'
     ];
